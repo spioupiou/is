f69956e4cb9ac591ddddbd8b38e8613284c56e4c
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-22 19:29:24 +0900

    Commented out prefecture/require:false in views so that there is no issue on sign-up for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   has_many :bookings
 
   validates :first_name, :last_name, presence: true
-  validates :prefecture, inclusion: { in: %w[Aichi Akita Aomori Chiba Ehime Fukui Fukuoka Fukushima Gifu Gunma Hiroshima
-    Hokkaido Hyogo Ibaraki Ishikawa Iwate Kagawa Kagoshima Kanagawa
-    Kochi Kumamoto Kyoto Mie Miyagi Miyazaki Nagano Nagasaki Nara Niigata
-    Okayama Okinawa Oita Osaka Saga Saitama Shiga Shimane Shizuoka Tochigi
-    Tokushima Tokyo Tottori Toyama Wakayama Yamagata Yamaguchi Yamanashi], allow_nil: false }
+  # validates :prefecture, inclusion: { in: %w[Aichi Akita Aomori Chiba Ehime Fukui Fukuoka Fukushima Gifu Gunma Hiroshima
+  #   Hokkaido Hyogo Ibaraki Ishikawa Iwate Kagawa Kagoshima Kanagawa
+  #   Kochi Kumamoto Kyoto Mie Miyagi Miyazaki Nagano Nagasaki Nara Niigata
+  #   Okayama Okinawa Oita Osaka Saga Saitama Shiga Shimane Shizuoka Tochigi
+  #   Tokushima Tokyo Tottori Toyama Wakayama Yamagata Yamaguchi Yamanashi], allow_nil: false }
 end
