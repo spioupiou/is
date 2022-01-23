d104d6c6594b33dcb75d8382e618e5d756c99f6d
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-23 19:00:36 +0900

    Bookings seeds + Kondo model association

--- app/models/kondo.rb
+++ app/models/kondo.rb
@@ -1,6 +1,7 @@
 class Kondo < ApplicationRecord
   belongs_to :user
   has_many :users, through: :bookings
+  has_many :bookings # no dependent: :destroy, provider can't delete services with pending bookings
 
   validates :name, :summary, :details, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # Create ourselves!
 puts "Destroying previous seeds"
+Booking.destroy_all
 Kondo.destroy_all
 User.destroy_all
 puts "previous seeds destroyed!"
@@ -8,27 +9,27 @@ puts "previous seeds destroyed!"
 
 puts "Creating Users"
 user1 = User.create!(
-            first_name: "Hiro", 
-            last_name: "Takemura", 
-            email: "hiro@email.com", 
+            first_name: "Hiro",
+            last_name: "Takemura",
+            email: "hiro@email.com",
             password: "123456"
 )
 user2 = User.create!(
-            first_name: "Cedrine", 
-            last_name: "Monnet", 
-            email: "cedrine@email.com", 
+            first_name: "Cedrine",
+            last_name: "Monnet",
+            email: "cedrine@email.com",
             password: "123456"
 )
 user3 = User.create!(
-            first_name: "Carl", 
-            last_name: "Noval", 
-            email: "carl@email.com", 
+            first_name: "Carl",
+            last_name: "Noval",
+            email: "carl@email.com",
             password: "123456"
 )
 user4 = User.create!(
-            first_name: "Shante", 
-            last_name: "Johnson", 
-            email: "shante@email.com", 
+            first_name: "Shante",
+            last_name: "Johnson",
+            email: "shante@email.com",
             password: "123456"
 )
 puts "User creation done!"
@@ -38,15 +39,29 @@ puts "User creation done!"
 puts "Creating kondos"
 
 20.times do
-    kondos =  Kondo.new(
-                name: ["Interior Designer","House Keeping", "Home Destroyer", "Thief"].sample,
-                summary: Faker::Lorem.sentence(word_count: 20, supplemental: true, random_words_to_add: 10),
-                details: Faker::Lorem.sentence(word_count: 50, supplemental: true, random_words_to_add: 50),
-                prefecture: ["Tokyo", "Kanagawa", "Chiba", "Saitama", "Ibaraki", "Tochigi", "Yamanashi"].sample,
-                price: ((rand(5..20)) * 1000),
-                user_id: [user1, user2, user3, user4].sample.id
-                )
-    kondos.save!
+  kondos = Kondo.new(
+              name: ["Interior Designer","House Keeping", "Home Destroyer", "Thief"].sample,
+              summary: Faker::Lorem.sentence(word_count: 20, supplemental: true, random_words_to_add: 10),
+              details: Faker::Lorem.sentence(word_count: 50, supplemental: true, random_words_to_add: 50),
+              prefecture: ["Tokyo", "Kanagawa", "Chiba", "Saitama", "Ibaraki", "Tochigi", "Yamanashi"].sample,
+              price: ((rand(5..20)) * 1000),
+              user_id: [user1, user2, user3, user4].sample.id
+              )
+  kondos.save!
 end
 
 puts "Kondos Creation done!"
+
+puts "Creating bookings"
+
+kondos = Kondo.all
+
+10.times do
+  bookings = Booking.new(
+    user_id: [user1, user2, user3, user4].sample.id,
+    kondo_id: kondos.sample.id,
+    confirmed: false,
+    booked_date: Time.now
+  )
+  bookings.save!
+end
