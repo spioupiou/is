5663977ee9002bcaeed16abebf2b73e34294df8c
tofusupreme <yunishbacon@gmail.com>
2022-01-26 15:44:39 +0900

    Updates to Index Pundit

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,9 +1,10 @@
 class KondosController < ApplicationController
   def index
-    @kondos = Kondo.order(created_at: :desc)
+     @kondos = Kondo.order(created_at: :desc)
   end
 
   def show
     @kondo = Kondo.find(params[:id])
   end
+
 end
---
+++ app/policies/kondo_policy.rb
@@ -0,0 +1,10 @@
+class KondoPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+    def index?
+      true
+  end
+end
+end
