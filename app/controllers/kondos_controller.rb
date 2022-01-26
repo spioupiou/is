a38c2d4cbdd346c796edf2da009942ba86def0bd
tofusupreme <yunishbacon@gmail.com>
2022-01-27 08:46:14 +0900

    Added Actions to Kondos Controller

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,9 +1,51 @@
 class KondosController < ApplicationController
+  before_action :set_kondo, only: %i[show edit update destroy]
+
   def index
     @kondos = policy_scope(Kondo).order(created_at: :desc)
   end
 
   def show
+  end
+
+  def new
+    @kondo = Kondo.new
+  end
+
+  def edit
+  end
+
+  def create
+    @kondo = Kondo.new(kondo_params)
+
+    if @kondo.save
+      redirect_to @kondo, notice: 'Kondo was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @kondo.update(kondo_params)
+    if @kondo.save
+      redirect_to @kondo, notice: 'Kondo was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kondo.destroy
+    redirect_to kondos_path, notice: 'Kondo was successfully destroyed.'
+  end
+
+  private
+
+  def set_kondo
     @kondo = Kondo.find(params[:id])
+  end
+
+  def kondo_params
+    params.require(:kondo).permit(:name, :summary, :details, :prefecture, :price)
   end
 end
