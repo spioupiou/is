29ea8fef3dd33db9e9b9759c32e9abbad49a2fe4
tofusupreme <yunishbacon@gmail.com>
2022-01-27 20:47:37 +0900

    Updated Kondos Controller

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,4 +1,6 @@
 class KondosController < ApplicationController
+before_action :set_kondo, only: %i[show edit update destroy]
+
   def index
     @kondos = Kondo.order(created_at: :desc)
     unless params[:search_kondos] == ""
@@ -9,6 +11,46 @@ class KondosController < ApplicationController
   end
 
   def show
+  end
+
+  def new
+    @kondo = Kondo.new
+  end
+
+  def edit
+  end
+
+  def create
+    @kondo = Kondo.new(kondo_params)
+
+    if @kondo.save
+      redirect_to @kondo, notice: 'Kondo was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @kondo.update(kondo_params)
+    if @kondo.save
+      redirect_to @kondo, notice: 'Kondo was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kondo.destroy
+    redirect_to kondos_path, notice: 'Kondo was successfully destroyed.'
+  end
+
+  private
+
+  def set_kondo
     @kondo = Kondo.find(params[:id])
+  end
+
+  def kondo_params
+    params.require(:kondo).permit(:name, :summary, :details, :prefecture, :price)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :kondos
   has_many :bookings
   has_one_attached :photo
-  
+
   # no need for validates :role, inclusion: { in: ["renter", "provider"] }
   # cause enum already takes care of this validation too
-  enum role: { provider: 'provider', renter: 'renter' }  
+  enum role: { provider: 'provider', renter: 'renter' }
 
   validates :first_name, :last_name, presence: true
   validates :prefecture,
