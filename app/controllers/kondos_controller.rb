e3f6f7969d4b666ab6ae8235cbd08cb1aa15c165
Hiroya Takemura <hirotakemura210@gmail.com>
2022-01-27 21:08:47 +0900

    implemented kondo-index for prvder

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,11 +1,21 @@
 class KondosController < ApplicationController
   def index
-    @kondos = Kondo.order(created_at: :desc)
-    unless params[:search_kondos] == ""
-      @kondos = @kondos.where("LOWER(prefecture) like ?","%#{params[:search_kondos].to_s.downcase}%")
-    else
-      @kondos = Kondo.all
+    #Renter's Page
+    if current_user.role == "renter"
+      @kondos = Kondo.order(created_at: :desc)
+
+      unless params[:search_kondos] == ""
+        @kondos = @kondos.where("LOWER(prefecture) like ?","%#{params[:search_kondos].to_s.downcase}%")
+      else
+        @kondos = Kondo.all
+      end
+
     end
+    #Provider's Page
+    if current_user.role == "provider"
+      @kondos = Kondo.where(user_id: current_user.id)
+    end
+
   end
 
   def show
