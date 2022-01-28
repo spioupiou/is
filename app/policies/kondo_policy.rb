dfd6488c896b6c914eeb7a01ba6db2fc6b3ebc85
tofusupreme <yunishbacon@gmail.com>
2022-01-28 11:26:35 +0900

    Added pundits to kondos

--- app/policies/kondo_policy.rb
+++ app/policies/kondo_policy.rb
@@ -3,5 +3,20 @@ class KondoPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+    def new?
+      current_user == 'provider'
+    end
+
+    def create?
+      current_user == 'provider'
+    end
+
+    def update?
+      current_user == 'provider'
+    end
+
+    def destroy?
+      current_user == 'provider'
+    end
   end
 end
