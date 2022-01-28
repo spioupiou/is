b9c832fd1d5242ecfc363be1f399ab420d47958f
Shante J <67456282+TofuSupreme@users.noreply.github.com>
2022-01-29 08:40:40 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -43,21 +43,8 @@ ActiveRecord::Schema.define(version: 2022_01_28_010808) do
     t.date "booked_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
     t.index ["kondo_id"], name: "index_bookings_on_kondo_id"
-    t.index ["slug"], name: "index_bookings_on_slug", unique: true
     t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "kondos", force: :cascade do |t|
