69e651947f0394c71702eb5aac8236a1f709f09f
Hiroya Takemura <hirotakemura210@gmail.com>
2022-01-31 10:38:30 +0900

    provider can update their kondo details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @kondos = Kondo.all.first(4)
+    @kondos = Kondo.all.sample(4)
   end
 
   def profile
