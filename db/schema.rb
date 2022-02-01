82e5f737075e1c951b2c0cf1d255200ee027d3c0
tofusupreme <yunishbacon@gmail.com>
2022-02-01 20:13:39 +0900

    Created profile migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_014006) do
+ActiveRecord::Schema.define(version: 2022_02_01_111057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,21 @@ ActiveRecord::Schema.define(version: 2022_01_30_014006) do
     t.date "booked_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["kondo_id"], name: "index_bookings_on_kondo_id"
+    t.index ["slug"], name: "index_bookings_on_slug", unique: true
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "kondos", force: :cascade do |t|
@@ -56,7 +69,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_014006) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "service_duration", default: 1, null: false
+    t.integer "service_duration", default: 1
     t.index ["user_id"], name: "index_kondos_on_user_id"
   end
 
@@ -83,6 +96,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_014006) do
     t.string "last_name"
     t.string "prefecture"
     t.string "role"
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
