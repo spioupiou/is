6e94a98ae7a89121da0865712733de740870558f
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-05 15:57:41 +0900

    Restricted search

--- app/models/kondo.rb
+++ app/models/kondo.rb
@@ -5,7 +5,7 @@ class Kondo < ApplicationRecord
   has_one_attached :photo
   acts_as_taggable_on :tags
 
-  include PgSearch::Model 
+  include PgSearch::Model
     pg_search_scope :global_search,
       against: [ :prefecture ],
       associated_against: {
@@ -16,11 +16,7 @@ class Kondo < ApplicationRecord
       }
 
   $categories = ["designers", "keepers", "others"]
-  $prefectures = %w[Aichi Akita Aomori Chiba Ehime Fukui Fukuoka Fukushima Gifu Gunma Hiroshima
-    Hokkaido Hyogo Ibaraki Ishikawa Iwate Kagawa Kagoshima Kanagawa
-    Kochi Kumamoto Kyoto Mie Miyagi Miyazaki Nagano Nagasaki Nara Niigata
-    Okayama Okinawa Oita Osaka Saga Saitama Shiga Shimane Shizuoka Tochigi
-    Tokushima Tokyo Tottori Toyama Wakayama Yamagata Yamaguchi Yamanashi]
-    
+  $prefectures = %w[Chiba Kyoto Osaka Tokyo]
+
   validates :name, :summary, :details, :service_duration, presence: true
 end
