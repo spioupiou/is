7e8553b1f6d27d167d7e9ab68d276bc4cda3d248
tofusupreme <yunishbacon@gmail.com>
2022-02-05 15:30:31 +0900

    updated profile page with reviews

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,11 @@ class ProfilesController < ApplicationController
   def show
       @user = User.find(params[:id])
       @profile = Profile.find(params[:id])
+      @kondos = Kondo.where(user_id: @user.id)
+      @bookings = Booking.where(kondo_id:@kondos.map(&:id))
+      @reviews = Review.where(booking_id: @bookings.map(&:id))
+
+
   end
 
   def edit
